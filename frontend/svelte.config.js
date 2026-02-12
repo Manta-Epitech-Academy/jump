4c4149faca6c6f2b172d277bfc54d0bbf3a6288e
Mateo Cabrera <mateo.cabrera@epitech.eu>
2026-02-12 14:20:24 +0100

    fix(frontend): switch to adapter-node for base path support

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'svelte-adapter-bun';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,6 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		paths: {
+			base: '/tekcamp'
+		},
 		alias: {
 			$lib: 'src/lib'
 		},
