429c3f7e3f70448dab2d23dc2f1318b2211083dc
Hugo-Fabresse <hugo.fabresse@epitech.eu>
2026-04-16 11:05:07 +0200

    fix(migrations): resolve conflicting constraint and column renames from merge

--- frontend/prisma/migrations/20260414150000_rename_external_fields_to_camel_case/migration.sql
+++ frontend/prisma/migrations/20260414150000_rename_external_fields_to_camel_case/migration.sql
@@ -5,3 +5,6 @@ ALTER INDEX "Campus_external_name_key" RENAME TO "Campus_externalName_key";
 -- Event: external_id -> externalId
 ALTER TABLE "Event" RENAME COLUMN "external_id" TO "externalId";
 ALTER INDEX "Event_external_id_key" RENAME TO "Event_externalId_key";
+
+-- Talent: salesforceId -> externalId
+ALTER TABLE "Talent" RENAME COLUMN "salesforceId" TO "externalId";
